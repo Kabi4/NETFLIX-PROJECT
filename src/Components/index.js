8ce205dcf4922bcaa56744ee175f5fce6525a102
Kushagra Singh <59528754+Kabi4@users.noreply.github.com>
2020-10-29 16:36:11 +0530

    Add files via upload

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,3 +2,5 @@ export {default as Welcome} from './Welcome/index';
 export {default as Jumbotron} from "./Jumbotron/index";
 export {default as Footer} from "./Footer/index";
 export {default as Optform} from "./Optform/index";
+export {default as Header} from './Header/index';
+export {default as Feature} from "./Features/index";
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React from "react";
 import JumbotronmMain from "../Container/Jumbotron/Jumbotron";
 import FooterMain from "../Container/Footer/Footer";
 import WelcomeMain from "../Container/Welcome/Welcome";
+import HeaderMain from "../Container/Header/Header";
+import { Optform, Feature } from "../Components";
 const Home = () => {
-    return (
-        <>
-          <JumbotronmMain />
-          <WelcomeMain />
-          <FooterMain />
-        </>
-    )
-}
+  return (
+    <>
+      <HeaderMain>
+        <Feature>
+          <Feature.Title>
+            Unlimited files, TV programmes and more.
+          </Feature.Title>
+          <Feature.Subtitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.Subtitle>
+          <Optform>
+            <Optform.Input placeholder="Email address" />
+            <Optform.Button>Try it now</Optform.Button>
+            <Optform.Break />
+            <Optform.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </Optform.Text>
+          </Optform>
+        </Feature>
+      </HeaderMain>
+      <JumbotronmMain />
+      <WelcomeMain />
+      <FooterMain />
+    </>
+  );
+};
 
-export default Home
+export default Home;
